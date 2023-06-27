cf72b54d178ac9d018930ef00a3e5b746bf84dd1
Aswin-S-T <aswinswebart15@gmail.com>
2023-06-27 16:51:19 +0530

    new commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,11 @@ function Header() {
   return (
     <div>
       <nav class="navbar navbar-light">
-        <span class="navbar-brand mb-0 h1 text-white">City Bus</span>
+        <span class="navbar-brand mb-0 h1 text-white">
+          <a href="/" style={{ textDecoration: "none", color: "white" }}>
+            City Bus
+          </a>
+        </span>
       </nav>
     </div>
   );
