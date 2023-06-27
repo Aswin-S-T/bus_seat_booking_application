a5b715931f70432ea93a6e4539c948ff71fe0a88
Aswin-S-T <aswinswebart15@gmail.com>
2023-06-27 18:09:41 +0530

    api integrated

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BUS_IMAGE, NO_IMAGE } from "../constats/images";
 import data from "../data/data";
+import axios from "axios";
+import { BACKEND_URL } from "../constats/Api";
+import LoadingBox from "./LoadingBox";
+import ErrorBox from "./ErrorBox";
 
 function Table() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [buses, setBuses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`${BACKEND_URL}/api/v1/user/bus`);
+        if (res && res.status == 200) {
+          setLoading(false);
+          setBuses(res.data.data);
+        }
+      } catch (error) {
+        setError(true);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <table class="table table-striped">
@@ -17,30 +40,36 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {data.buses.map((bus, index) => (
-            <>
-              <tr>
-                <th scope="row">{index + 1}</th>
-                <td>
-                  {bus.bus_name}
-                  <img
-                    src={bus.imageUrl ? bus.imageUrl : NO_IMAGE}
-                    className="thumb"
-                    style={{ marginLeft: "10px" }}
-                  />
-                </td>
-                <td>
-                  {bus.from} - {bus.to}
-                </td>
-                <td>{bus.time}</td>
-                <td>
-                  <a href={`/details/${bus._id}`}>
-                    <button className="bookBtn">Book Now</button>
-                  </a>
-                </td>
-              </tr>
-            </>
-          ))}
+          {loading ? (
+            <LoadingBox />
+          ) : error ? (
+            <ErrorBox />
+          ) : (
+            buses.map((bus, index) => (
+              <>
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>
+                    {bus.bus_name}
+                    <img
+                      src={bus.imageUrl ? bus.imageUrl : NO_IMAGE}
+                      className="thumb"
+                      style={{ marginLeft: "10px" }}
+                    />
+                  </td>
+                  <td>
+                    {bus.from} - {bus.to}
+                  </td>
+                  <td>{bus.time}</td>
+                  <td>
+                    <a href={`/details/${bus._id}`}>
+                      <button className="bookBtn">Book Now</button>
+                    </a>
+                  </td>
+                </tr>
+              </>
+            ))
+          )}
         </tbody>
       </table>
     </div>
