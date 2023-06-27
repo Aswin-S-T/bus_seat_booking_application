a037d4c7796b7f77aaf0ff4f47befc2c6c5a52a1
Aswin-S-T <aswinswebart15@gmail.com>
2023-06-27 18:18:13 +0530

    details screen api integrated

--- frontend/src/screens/BusDeatailScreen.js
+++ frontend/src/screens/BusDeatailScreen.js
@@ -3,17 +3,28 @@ import SeatLayout from "../components/SeatLayout";
 import StripeCheckoutButton from "../components/StripeCheckoutButton";
 import data from "../data/data";
 import { useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import ErrorBox from "../components/ErrorBox";
+import axios from "axios";
+import { BACKEND_URL } from "../constats/Api";
 
 function BusDeatailScreen() {
-  const [busDetails, setBusDetails] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [bus, setBuses] = useState({});
   const { id } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
-      const res = await data.buses;
-      let details = res.find((bus) => bus._id == id);
-      if (details) {
-        setBusDetails(details);
+      try {
+        setLoading(true);
+        const res = await axios.get(`${BACKEND_URL}/api/v1/user/bus/${id}`);
+        if (res && res.status == 200) {
+          setLoading(false);
+          setBuses(res.data.data);
+        }
+      } catch (error) {
+        setError(true);
       }
     };
     fetchData();
@@ -21,59 +32,63 @@ function BusDeatailScreen() {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img src={busDetails?.imageUrl} className="w-100 mt-5" />
-        </div>
-        <div className="col-md-6">
-          <div>
-            <div className="row">
-              <div className="col-md-6">
-                <p>Full Name</p>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-md-6">
-                <p>Mobile Number / Email</p>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-md-6">
-                <p>Starting Point</p>
-                <select className="form-control">
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                </select>
-              </div>
-              <div className="col-md-6">
-                <p>Destination Point</p>
-                <select className="form-control">
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                  <option>sd</option>
-                </select>
-              </div>
-              <div className="col-md-12">
-                <p>Travelling Date</p>
-                <input type="date" className="form-control" />
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <ErrorBox />
+      ) : (
+        <div className="row">
+          <div className="col-md-6 mt-5">
+            <img src={bus?.imageUrl} className="w-100 mt-5" />
+          </div>
+          <div className="col-md-6">
+            <div>
+              <div className="row">
+                <div className="col-md-6">
+                  <p>Full Name</p>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col-md-6">
+                  <p>Mobile Number / Email</p>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="col-md-6">
+                  <p>Starting Point</p>
+                  <select className="form-control">
+                    {bus && bus.routes && bus.routes.length > 0 ? (
+                      bus.routes.map((route) => <option>{route}</option>)
+                    ) : (
+                      <h1>No starting point found</h1>
+                    )}
+                  </select>
+                </div>
+                <div className="col-md-6">
+                  <p>Destination Point</p>
+                  <select className="form-control">
+                    {bus && bus.routes && bus.routes.length > 0 ? (
+                      bus.routes.map((route) => <option>{route}</option>)
+                    ) : (
+                      <h1>No Destination point found</h1>
+                    )}
+                  </select>
+                </div>
+                <div className="col-md-12">
+                  <p>Travelling Date</p>
+                  <input type="date" className="form-control" />
+                </div>
               </div>
             </div>
-          </div>
-          <SeatLayout />
-          <div className="mt-3">
-            <input type="checkbox" /> Continue with payment (Once you clicked
-            this, you can't change the option)
-          </div>
-          <div>
-            <StripeCheckoutButton price={232} />
+            <SeatLayout />
+            <div className="mt-3">
+              <input type="checkbox" /> Continue with payment (Once you clicked
+              this, you can't change the option)
+            </div>
+            <div>
+              <StripeCheckoutButton price={232} />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
