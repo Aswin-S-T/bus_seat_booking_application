7e140718edad7e443d1dd62a1f6837bcb7f766c0
Aswin-S-T <aswinswebart15@gmail.com>
2023-07-11 01:11:25 +0530

    generate invoice funcionality done

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -7,7 +7,56 @@ const {
 } = require("../controllers/users/userController");
 const Bus = require("../models/busModel");
 const { generateBusDetails } = require("../utils/helper");
+const nodemailer = require("nodemailer");
+const pdfkit = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+
 const userRouter = express.Router();
+
+function generateInvoice() {
+  const pdfContent = `
+    <html>
+      <head>
+        <style>
+          /* Add your invoice styling here */
+        </style>
+      </head>
+      <body>
+        <h1>Invoice</h1>
+        <p>Invoice content goes here</p>
+      </body>
+    </html>
+  `;
+  const dummyPdf = Buffer.from(pdfContent);
+
+  return dummyPdf;
+}
+
+function generateInvoice() {
+  const PDFDocument = require("pdfkit");
+  const fs = require("fs");
+
+  const invoiceNumber = "INV-001";
+  const invoiceDate = new Date().toLocaleDateString();
+  const customerName = "John Doe";
+  const totalAmount = 100.0;
+
+  const doc = new PDFDocument();
+
+  doc.text("Invoice", { align: "center" });
+  doc.moveDown();
+  doc.text("Item 1: $10");
+  doc.text("Item 2: $20");
+  doc.text("Total: $30");
+  doc.end();
+
+  const invoice = fs.readFileSync("invoice.pdf");
+
+  fs.unlinkSync("invoice.pdf");
+
+  return invoice;
+}
 
 userRouter.get("/", (req, res) => {
   res.send("user router api callled");
@@ -43,6 +92,71 @@ userRouter.post("/add-bus-details", async (req, res) => {
   let busData = req.body;
   addBusDetails(busData).then((result) => {
     res.send(result);
+  });
+});
+
+userRouter.post("/sendmail", async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail", // e.g., 'Gmail' or 'Outlook'
+    auth: {
+      user: "aswin1542000@gmail.com",
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  // Setup email data
+  const mailOptions = {
+    from: "aswinst154@gmail.com",
+    to: "aswin1542000@gmail.com",
+    subject: "Hello from Nodemailer",
+    text: "Hello, this is a test email sent using Nodemailer!",
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error occurred:", error.message);
+    } else {
+      console.log("Email sent successfully!");
+      console.log("Message ID:", info.messageId);
+    }
+  });
+  res.send("hello");
+});
+
+userRouter.get("/send-invoice", async (req, res) => {
+  const invoice = generateInvoice();
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: "Invoice",
+    text: "Please find attached invoice.",
+    attachments: [
+      {
+        filename: "invoice.pdf",
+        content: invoice,
+        contentType: "application/pdf",
+      },
+    ],
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("Error occurred while sending email.");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Email sent successfully!");
+    }
   });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ db.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Routes configuration
 app.use("/api/v1/user", userRouter);
