30dbac9f3a9c91c53dc5fc7dc9da7f4b8883471e
Aswin-S-T <aswinswebart15@gmail.com>
2023-07-13 15:00:34 +0530

    cors done

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,19 @@ const userRouter = require("./routes/userRouter");
 const companyRouter = require("./routes/companies/comapnyRouter");
 db.connect();
 
+const frontendURL =
+  "https://64afc2c090cb4b3dc09a9398--graceful-parfait-655225.netlify.app/";
+
+const corsOptions = {
+  origin: frontendURL,
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // Routes configuration
