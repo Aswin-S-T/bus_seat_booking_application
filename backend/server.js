06b4748975abc5188c804da87b30ee4dd97ee938
Aswin-S-T <aswinswebart15@gmail.com>
2023-07-13 15:23:11 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -13,29 +13,10 @@ const userRouter = require("./routes/userRouter");
 const companyRouter = require("./routes/companies/comapnyRouter");
 db.connect();
 
-// const corsOptions = {
-//   origin: "https://64afc6878ddcda4252e3359c--dancing-blini-b82d38.netlify.app/", // Replace with your frontend URL
-//   optionsSuccessStatus: 200, // Some legacy browsers (e.g., IE11) choke on 204
-// };
-
-// app.use(cors());
-
-// Enable CORS with specific options
-app.use(
-  cors({
-    origin:
-      "https://64afc8cdccf19b00a45aeabd--extraordinary-pegasus-04b5a7.netlify.app/", // Replace with your allowed origin
-    methods: ["GET", "POST"], // Replace with your allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Replace with your allowed headers
-  })
-);
-
-
-
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // Routes configuration
